21717d63d962ebfd8c426a8385d52f5b7ee8bede
Gaspi <gaspar_peralta09@hotmail.com>
2026-02-27 08:24:07 +0100

    Final CORS + security headers fix for FCC

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Credentials", "true");
     res.setHeader("Vary", "Origin");
   } else {
-    res.setHeader("Access-Control-Allow-Origin", "*");
+
   }
 
   res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS");
